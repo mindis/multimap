d1d0a6c509c862ae248232f73a1279ada4ab23a3
Martin Trenkmann <martin.trenkmann@posteo.de>
2015-11-18 22:55:32 +0100

    Kick out boost_program_options

--- src/cpp/multimap/main-tool.cpp
+++ src/cpp/multimap/main-tool.cpp
@@ -15,127 +15,333 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <cstdio>
 #include <iostream>
-#include <boost/program_options.hpp>
 #include "multimap/thirdparty/mt/mt.hpp"
-#include "multimap/Map.hpp"
+#include "multimap/operations.hpp"
+
+// void requireOption(const po::variables_map& arguments, const char* option) {
+//  if (arguments.count(option) == 0) {
+//    mt::throwRuntimeErrorFormat("Option '--%s' is missing.", option);
+//  }
+//}
+
+// void runHelpCommand(const po::options_description& descriptions) {
+//  std::cout << descriptions << "\n";
+//}
+
+// void runImportCommand(const po::variables_map& arguments) {
+//  requireOption(arguments, SOURCE);
+//  requireOption(arguments, TARGET);
+
+//  multimap::Options options;
+//  if (arguments.count(CREATE)) {
+//    options.create_if_missing = true;
+//    if (arguments.count(BS)) {
+//      options.block_size = arguments[BS].as<std::size_t>();
+//    }
+//    if (arguments.count(NSHARDS)) {
+//      options.num_shards = arguments[NSHARDS].as<std::size_t>();
+//    }
+//  }
+
+//  multimap::Map::importFromBase64(arguments[TARGET].as<std::string>(),
+//                                  arguments[SOURCE].as<std::string>(),
+//                                  options);
+//}
+
+// void runExportCommand(const po::variables_map& arguments) {
+//  requireOption(arguments, SOURCE);
+//  requireOption(arguments, TARGET);
+
+//  multimap::Map::exportToBase64(arguments[SOURCE].as<std::string>(),
+//                                arguments[TARGET].as<std::string>());
+//}
+
+// void runOptimizeCommand(const po::variables_map& arguments) {
+//  requireOption(arguments, SOURCE);
+//  requireOption(arguments, TARGET);
+
+//  multimap::Options options;
+//  if (arguments.count(BS)) {
+//    options.block_size = arguments[BS].as<std::size_t>();
+//  } else {
+//    options.block_size = 0; // Use same block_size as SOURCE.
+//  }
+//  if (arguments.count(NSHARDS)) {
+//    options.num_shards = arguments[NSHARDS].as<std::size_t>();
+//  } else {
+//    options.num_shards = 0; // Use same num_shards as SOURCE.
+//  }
+
+//  multimap::Map::optimize(arguments[SOURCE].as<std::string>(),
+//                          arguments[TARGET].as<std::string>(), options);
+//}
+
+// multimap <command> <options> <args>
+
+// multimap help
+// multimap stats    <path/to/map>
+// multimap import   <options> <path/to/map> <path/to/file_or_directory>
+// multimap export   <path/to/map> <path/to/file>
+// multimap optimize <options> <path/to/map> <path/to/optimized_map>
+// int main(int argc, const char** argv) {
+//  po::positional_options_description positional_options;
+//  positional_options.add(COMMAND, 1).add(PATH1, 1).add(PATH2, 1);
+
+//  multimap::Options options;
+//  po::options_description options_description("Options");
+//  options_description.add_options()(
+//        NSHARDS, po::value<std::size_t>(),
+//        "Number of shards to use for a new instance. "
+//        "Default is " + std::to_string(options.num_shards) + '.').c_str())
+
+//  // Options
+////  po::options_description commands_description("Options");
+////  commands_description.add_options()(HELP, "Print this help message and
+/// exit.")(
+////      IMPORT, "Import key-value pairs from Base64-encoded text files.")(
+////      EXPORT, "Export key-value pairs to a Base64-encoded text file.")(
+////      OPTIMIZE, "Rewrite an instance performing various optimizations.");
+
+////  multimap::Options options;
+////  po::options_description options_description("OPTIONS", 100);
+////  options_description.add_options()(SOURCE, po::value<std::string>(),
+////                                    "Directory or file used as input.")(
+////      TARGET, po::value<std::string>(), "Directory or file used as
+/// output.")(
+////      CREATE, "Create new instance if missing.")(
+////      BS, po::value<std::size_t>(),
+////      ("Block size to use for a new instance. "
+////       "Default is " +
+////       std::to_string(options.block_size) +
+////       '.').c_str())(NSHARDS, po::value<std::size_t>(),
+////                     ("Number of shards to use for a new instance. "
+////                      "Default is " +
+////                      std::to_string(options.num_shards) + '.').c_str());
+
+//  po::options_description descriptions;
+//  descriptions.add(commands_description).add(options_description);
+
+//  po::variables_map arguments;
+//  po::store(po::command_line_parser(argc, argv)
+//                .options(descriptions)
+//                .positional(positional_options)
+//                .run(),
+//            arguments);
+//  po::notify(arguments);
+
+//  try {
+//    if (arguments.count(HELP)) {
+//      runHelpCommand(descriptions);
+//    } else if (arguments.count(IMPORT)) {
+//      runImportCommand(arguments);
+//    } else if (arguments.count(EXPORT)) {
+//      runExportCommand(arguments);
+//    } else if (arguments.count(OPTIMIZE)) {
+//      runOptimizeCommand(arguments);
+//    } else {
+//      std::cout << "Try with --" << HELP << '\n';
+//    }
+//  } catch (std::exception& error) {
+//    std::cerr << error.what() << '\n';
+//    return EXIT_FAILURE;
+//  }
+//  return EXIT_SUCCESS;
+
+//  // Add Copyright notice
+//  // Add link to website for more infor about params.
+//}
 
 const char* HELP     = "help";
+const char* STATS    = "stats";
 const char* IMPORT   = "import";
 const char* EXPORT   = "export";
 const char* OPTIMIZE = "optimize";
 
-const char* SOURCE   = "source";
-const char* TARGET   = "target";
-const char* CREATE   = "create";
-const char* BS       = "bs";
-const char* NSHARDS  = "nshards";
+const char* BS       = "--bs";
+const char* CREATE   = "--create";
+const char* NSHARDS  = "--nshards";
 
-namespace po = boost::program_options;
+const auto COMMANDS  = { HELP, STATS, IMPORT, EXPORT, OPTIMIZE };
+const auto OPTIONS   = { BS, CREATE, NSHARDS };
 
-void requireOption(const po::variables_map& arguments, const char* option) {
-  if (arguments.count(option) == 0) {
-    mt::throwRuntimeErrorFormat("Option '--%s' is missing.", option);
-  }
+struct CommandLine {
+  std::string command, map, path;
+  std::map<std::string, std::string> options;
+};
+
+bool isCommand(const std::string& argument) {
+  return std::find(std::begin(COMMANDS), std::end(COMMANDS), argument) !=
+         std::end(COMMANDS);
 }
 
-void runHelpCommand(const po::options_description& descriptions) {
-  std::cout << descriptions << "\n";
+bool isOption(const std::string& argument) {
+  return std::find(std::begin(OPTIONS), std::end(OPTIONS), argument) !=
+         std::end(OPTIONS);
 }
 
-void runImportCommand(const po::variables_map& arguments) {
-  requireOption(arguments, SOURCE);
-  requireOption(arguments, TARGET);
-
-  multimap::Options options;
-  if (arguments.count(CREATE)) {
-    options.create_if_missing = true;
-    if (arguments.count(BS)) {
-      options.block_size = arguments[BS].as<std::size_t>();
+CommandLine parseCommandLine(int argc, const char** argv) {
+  CommandLine cmd_line;
+  const auto end = std::next(argv, argc);
+  auto it = std::next(argv);
+  if (it != end) {
+    mt::check(isCommand(*it), "Expected COMMAND when reading '%s'.", *it);
+    cmd_line.command = *it++;
+    if (it != end) {
+      mt::check(!isOption(*it), "Expected MAP when reading '%s'.", *it);
+      cmd_line.map = *it++;
+      if (it != end) {
+        if (!isOption(*it)) {
+          cmd_line.path = *it++;
+        }
+        while (it != end) {
+          if (*it == CREATE) {
+            cmd_line.options[*it++];
+            continue;
+          }
+          if (*it == BS) {
+            const auto option = *it++;
+            mt::check(it != end, "Missing argument for '%s'.", option);
+            cmd_line.options[option] = *it;
+            continue;
+          }
+          if (*it == NSHARDS) {
+            const auto option = *it++;
+            mt::check(it != end, "Missing argument for '%s'.", option);
+            cmd_line.options[option] = *it;
+            continue;
+          }
+          mt::failFormat("Expected option when reading '%s'.", *it);
+        }
+      }
     }
-    if (arguments.count(NSHARDS)) {
-      options.num_shards = arguments[NSHARDS].as<std::size_t>();
-    }
   }
 
-  multimap::Map::importFromBase64(arguments[TARGET].as<std::string>(),
-                                  arguments[SOURCE].as<std::string>(), options);
+  return cmd_line;
 }
 
-void runExportCommand(const po::variables_map& arguments) {
-  requireOption(arguments, SOURCE);
-  requireOption(arguments, TARGET);
-
-  multimap::Map::exportToBase64(arguments[SOURCE].as<std::string>(),
-                                arguments[TARGET].as<std::string>());
-}
-
-void runOptimizeCommand(const po::variables_map& arguments) {
-  requireOption(arguments, SOURCE);
-  requireOption(arguments, TARGET);
-
+multimap::Options initOptions(const CommandLine& cmd_line) {
   multimap::Options options;
-  if (arguments.count(BS)) {
-    options.block_size = arguments[BS].as<std::size_t>();
-  } else {
-    options.block_size = 0; // Use same block_size as SOURCE.
+  options.create_if_missing = cmd_line.options.count(CREATE);
+  if (cmd_line.options.count(BS)) {
+    options.block_size = std::stoul(cmd_line.options.at(BS));
   }
-  if (arguments.count(NSHARDS)) {
-    options.num_shards = arguments[NSHARDS].as<std::size_t>();
-  } else {
-    options.num_shards = 0; // Use same num_shards as SOURCE.
+  if (cmd_line.options.count(NSHARDS)) {
+    options.num_shards = std::stoul(cmd_line.options.at(NSHARDS));
   }
-
-  multimap::Map::optimize(arguments[SOURCE].as<std::string>(),
-                          arguments[TARGET].as<std::string>(), options);
+  return options;
 }
 
-int main(int argc, const char* argv[]) {
-  po::options_description commands_description("COMMANDS");
-  commands_description.add_options()(
-      HELP, "Print this help message and exit.")(
-      IMPORT, "Import key-value pairs from Base64-encoded text files.")(
-      EXPORT, "Export key-value pairs to a Base64-encoded text file.")(
-      OPTIMIZE, "Rewrite an instance performing various optimizations.");
+void runHelpCommand(const char* tool_name) {
+  const multimap::Options default_options;
+  std::printf(
+      "USAGE\n"
+      "\n  %s COMMAND MAP [PATH] [OPTIONS]"
+      "\n\nCOMMANDS\n"
+      "\n  %-10s     Print this help message and exit."
+      "\n  %-10s     Print statistics about an instance."
+      "\n  %-10s     Import key-value pairs from Base64-encoded text files."
+      "\n  %-10s     Export key-value pairs to a Base64-encoded text file."
+      "\n  %-10s     Rewrite an instance performing various optimizations."
+      "\n\nOPTIONS\n"
+      "\n  %-9s      Create a new instance if missing when importing data."
+      "\n  %-9s NUM  Block size to use for a new instance. Default is %lu."
+      "\n  %-9s NUM  Number of shards to use for a new instance. Default is "
+      "%lu."
+      "\n\nEXAMPLES\n"
+      "\n  %s %-8s path/to/map"
+      "\n  %s %-8s path/to/map path/to/input"
+      "\n  %s %-8s path/to/map path/to/input/base64.csv"
+      "\n  %s %-8s path/to/map path/to/input/base64.csv %s"
+      "\n  %s %-8s path/to/map path/to/output/base64.csv"
+      "\n  %s %-8s path/to/map path/to/output"
+      "\n  %s %-8s path/to/map path/to/output %s 128"
+      "\n  %s %-8s path/to/map path/to/output %s 46"
+      "\n  %s %-8s path/to/map path/to/output %s 46 %s 128"
+      "\n\n"
+      "\nCopyright (C) 2015 Martin Trenkmann"
+      "\n<http://multimap.io>\n",
+      tool_name,
+      HELP,
+      STATS,
+      IMPORT,
+      EXPORT,
+      OPTIMIZE,
+      CREATE,
+      BS, default_options.block_size,
+      NSHARDS, default_options.num_shards,
+      tool_name, STATS,
+      tool_name, IMPORT,
+      tool_name, IMPORT,
+      tool_name, IMPORT, CREATE,
+      tool_name, EXPORT,
+      tool_name, OPTIMIZE,
+      tool_name, OPTIMIZE, BS,
+      tool_name, OPTIMIZE, NSHARDS,
+      tool_name, OPTIMIZE, NSHARDS, BS);
+}
 
-  multimap::Options options;
-  po::options_description options_description("OPTIONS", 100);
-  options_description.add_options()(
-      SOURCE, po::value<std::string>(), "Directory or file used as input.")(
-      TARGET, po::value<std::string>(), "Directory or file used as output.")(
-      CREATE, "Create new instance if missing.")(
-      BS, po::value<std::size_t>(),
-        ("Block size to use for a new instance. "
-         "Default is " + std::to_string(options.block_size) + '.').c_str())(
-      NSHARDS, po::value<std::size_t>(),
-        ("Number of shards to use for a new instance. "
-         "Default is " + std::to_string(options.num_shards) + '.').c_str());
+void runStatsCommand(const CommandLine& cmd_line) {
+//  std::cout << multimap::stats(cmd_line.map) << std::endl;
+}
 
-  po::options_description descriptions;
-  descriptions.add(commands_description).add(options_description);
+void runImportCommand(const CommandLine& cmd_line) {
+  const auto options = initOptions(cmd_line);
+  multimap::importFromBase64(cmd_line.map, cmd_line.path, options);
+}
 
-  po::variables_map arguments;
-  po::store(po::parse_command_line(argc, argv, descriptions), arguments);
-  po::notify(arguments);
+void runExportCommand(const CommandLine& cmd_line) {
+  multimap::exportToBase64(cmd_line.map, cmd_line.path);
+}
 
-  try {
-    if (arguments.count(HELP)) {
-      runHelpCommand(descriptions);
-    } else if (arguments.count(IMPORT)) {
-      runImportCommand(arguments);
-    } else if (arguments.count(EXPORT)) {
-      runExportCommand(arguments);
-    } else if (arguments.count(OPTIMIZE)) {
-      runOptimizeCommand(arguments);
-    } else {
-      std::cout << "Try with --" << HELP << '\n';
-    }
-  } catch (std::exception& error) {
-    std::cerr << error.what() << '\n';
+void runOptimizeCommand(const CommandLine& cmd_line) {
+  const auto options = initOptions(cmd_line);
+  multimap::optimize(cmd_line.map, cmd_line.path, options);
+}
+
+int main(int argc, const char** argv) {
+  if (argc < 2) {
+    runHelpCommand(*argv);
     return EXIT_FAILURE;
   }
-  return EXIT_SUCCESS;
 
-  // Add Copyright notice
-  // Add link to website for more infor about params.
+  CommandLine cmd_line;
+  try {
+    cmd_line = parseCommandLine(argc, argv);
+  } catch (std::exception& error) {
+    std::cerr << "Invalid command line: " << error.what()
+              << "\nTry " << *argv << ' ' << HELP << std::endl;
+  }
+
+  try {
+    if (cmd_line.command == HELP) {
+      runHelpCommand(*argv);
+      return EXIT_SUCCESS;
+    }
+
+    if (cmd_line.command == STATS) {
+      runStatsCommand(cmd_line);
+      return EXIT_SUCCESS;
+    }
+
+    if (cmd_line.command == IMPORT) {
+      runImportCommand(cmd_line);
+      return EXIT_SUCCESS;
+    }
+
+    if (cmd_line.command == EXPORT) {
+      runExportCommand(cmd_line);
+      return EXIT_SUCCESS;
+    }
+
+    if (cmd_line.command == OPTIMIZE) {
+      runOptimizeCommand(cmd_line);
+      return EXIT_SUCCESS;
+    }
+
+  } catch (std::exception& error) {
+    std::cerr << error.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
