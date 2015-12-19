efb0a7b16fb5fa1e9ca4bbec2b3ef1683898375f
Martin Trenkmann <martin.trenkmann@posteo.de>
2015-12-19 15:09:29 +0100

    Apply Google C++ formatting

--- src/cpp/multimap/main.cpp
+++ src/cpp/multimap/main.cpp
@@ -19,8 +19,8 @@
 #include <cinttypes>
 #include <iostream>
 #include <boost/filesystem/operations.hpp>
-#include "multimap/thirdparty/mt/mt.hpp"
-#include "multimap/Map.hpp"
+#include <multimap/thirdparty/mt/mt.hpp>
+#include <multimap/Map.hpp>
 
 const auto HELP     = "help";
 const auto STATS    = "stats";
@@ -117,38 +117,45 @@ multimap::Options initOptions(const CommandLine& cmd) {
 void runHelpCommand(const char* tool_name) {
   const multimap::Options default_options;
   std::printf(
-      "USAGE\n"
-      "\n  %s COMMAND MAP [PATH] [OPTIONS]"
-      "\n\nCOMMANDS\n"
-      "\n  %-10s     Print this help message and exit."
-      "\n  %-10s     Print statistics about an instance."
-      "\n  %-10s     Import key-value pairs from Base64-encoded text files."
-      "\n  %-10s     Export key-value pairs to a Base64-encoded text file."
-      "\n  %-10s     Rewrite an instance performing various optimizations."
-      "\n\nOPTIONS\n"
-      "\n  %-9s      Create a new instance if missing when importing data."
-      "\n  %-9s NUM  Block size to use for a new instance. Default is %lu."
-      "\n  %-9s NUM  Number of shards to use for a new instance. Default is "
-      "%lu."
-      "\n  %-9s      Don't print out any status messages."
-      "\n\nEXAMPLES\n"
-      "\n  %s %-8s path/to/map"
-      "\n  %s %-8s path/to/map path/to/input"
-      "\n  %s %-8s path/to/map path/to/input/base64.csv"
-      "\n  %s %-8s path/to/map path/to/input/base64.csv %s"
-      "\n  %s %-8s path/to/map path/to/output/base64.csv"
-      "\n  %s %-8s path/to/map path/to/output"
-      "\n  %s %-8s path/to/map path/to/output %s 128"
-      "\n  %s %-8s path/to/map path/to/output %s 46"
-      "\n  %s %-8s path/to/map path/to/output %s 46 %s 128"
-      "\n\n"
-      "\nCopyright (C) 2015 Martin Trenkmann"
-      "\n<http://multimap.io>\n",
-      tool_name, HELP, STATS, IMPORT, EXPORT, OPTIMIZE, CREATE, BS,
-      default_options.block_size, NSHARDS, default_options.num_shards, QUIET,
-      tool_name, STATS, tool_name, IMPORT, tool_name, IMPORT, tool_name, IMPORT,
-      CREATE, tool_name, EXPORT, tool_name, OPTIMIZE, tool_name, OPTIMIZE, BS,
-      tool_name, OPTIMIZE, NSHARDS, tool_name, OPTIMIZE, NSHARDS, BS);
+    "USAGE\n"
+    "\n  %s COMMAND MAP [PATH] [OPTIONS]"
+    "\n\nCOMMANDS\n"
+    "\n  %-10s     Print this help message and exit."
+    "\n  %-10s     Print statistics about an instance."
+    "\n  %-10s     Import key-value pairs from Base64-encoded text files."
+    "\n  %-10s     Export key-value pairs to a Base64-encoded text file."
+    "\n  %-10s     Rewrite an instance performing various optimizations."
+    "\n\nOPTIONS\n"
+    "\n  %-9s      Create a new instance if missing when importing data."
+    "\n  %-9s NUM  Block size to use for a new instance. Default is %lu."
+    "\n  %-9s NUM  Number of shards to use for a new instance. Default is %lu."
+    "\n  %-9s      Don't print out any status messages."
+    "\n\nEXAMPLES\n"
+    "\n  %s %-8s path/to/map"
+    "\n  %s %-8s path/to/map path/to/input"
+    "\n  %s %-8s path/to/map path/to/input/base64.csv"
+    "\n  %s %-8s path/to/map path/to/input/base64.csv %s"
+    "\n  %s %-8s path/to/map path/to/output/base64.csv"
+    "\n  %s %-8s path/to/map path/to/output"
+    "\n  %s %-8s path/to/map path/to/output %s 128"
+    "\n  %s %-8s path/to/map path/to/output %s 46"
+    "\n  %s %-8s path/to/map path/to/output %s 46 %s 128"
+    "\n\n"
+    "\nCopyright (C) 2015 Martin Trenkmann"
+    "\n<http://multimap.io>\n",
+    tool_name, HELP, STATS, IMPORT, EXPORT, OPTIMIZE,
+    CREATE, BS, default_options.block_size,
+    NSHARDS, default_options.num_shards,
+    QUIET,
+    tool_name, STATS,
+    tool_name, IMPORT,
+    tool_name, IMPORT,
+    tool_name, IMPORT, CREATE,
+    tool_name, EXPORT,
+    tool_name, OPTIMIZE,
+    tool_name, OPTIMIZE, BS,
+    tool_name, OPTIMIZE, NSHARDS,
+    tool_name, OPTIMIZE, NSHARDS, BS);
 }
 
 void runStatsCommand(const CommandLine& cmd) {
@@ -202,14 +209,10 @@ void runExportCommand(const CommandLine& cmd) {
 void runOptimizeCommand(const CommandLine& cmd) {
   auto options = initOptions(cmd);
   if (cmd.options.count(BS) == 0) {
-    options.block_size = 0;
-    // If no BS is explicitly given set `block_size` to zero to indicate
-    // that the old block size should be used for the new optimized map.
+    options.keep_block_size();
   }
   if (cmd.options.count(NSHARDS) == 0) {
-    options.num_shards = 0;
-    // If no NSHARDS is explicitly given set `num_shards` to zero to indicate
-    // that the old number of shards should be used for the new optimized map.
+    options.keep_num_shards();
   }
   multimap::Map::optimize(cmd.map, cmd.path, options);
 }
