e7c019ea4774c5a432f6ebfe5177d02c95bf0e70
Martin Trenkmann <martin.trenkmann@posteo.de>
2016-01-04 21:49:59 +0100

    Rename file

---
+++ build/generate-jni-headers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Generates C/C++ header files for JNI usage.
+
+CLASSPATH=../bin
+CLASSES="io.multimap.Iterator io.multimap.Map"
+OUTPUT_DIR=../src/cpp/multimap/jni/generated
+
+javah -d $OUTPUT_DIR -jni -classpath $CLASSPATH $CLASSES
+shopt -s extglob
+rm $OUTPUT_DIR/!(*Native*)
+
+# Fix wrongly generated function names that contain inner class names.
+sed -i 's/Callables_Predicate/Callables_00024Predicate/g' $OUTPUT_DIR/*Native.h
+sed -i 's/Callables_Procedure/Callables_00024Procedure/g' $OUTPUT_DIR/*Native.h
+sed -i 's/Callables_Function/Callables_00024Function/g' $OUTPUT_DIR/*Native.h
+sed -i 's/Callables_LessThan/Callables_00024LessThan/g' $OUTPUT_DIR/*Native.h
