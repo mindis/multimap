51b817c45c97975acd13052b0a8e7691a74ded9c
Martin <martin.trenkmann@posteo.de>
2016-01-30 22:51:00 +0100

    Initial commit

---
+++ make-java-library.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script compiles Java source code and packs it into a single JAR file.
+
+set -e  # Exit on error
+# set -x  # Display commands
+
+BUILD_DIR=classes
+PACKAGE=io/multimap
+SOURCE_DIR=src/java
+SOURCE_FILES=($SOURCE_DIR/$PACKAGE/Callables.java \
+              $SOURCE_DIR/$PACKAGE/Check.java \
+              $SOURCE_DIR/$PACKAGE/Iterator.java \
+              $SOURCE_DIR/$PACKAGE/Map.java \
+              $SOURCE_DIR/$PACKAGE/Options.java)
+
+mkdir $BUILD_DIR
+javac -sourcepath $SOURCE_DIR -d $BUILD_DIR ${SOURCE_FILES[*]}
+
+MAJOR_VERSION=$(grep MAJOR_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
+MINOR_VERSION=$(grep MINOR_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
+BUILD_VERSION=$(grep BUILD_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
+JAR_FILE=multimap-$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION.jar
+
+jar cf $JAR_FILE $BUILD_DIR
+rm -rf $BUILD_DIR
