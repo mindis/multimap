f6f1337b9c28b6eaa5f547b9a725e7d3a6a4ed22
Martin <martin.trenkmann@posteo.de>
2016-02-01 21:09:38 +0100

    Fix wrong package bug

--- make-multimap-jar.sh
+++ make-multimap-jar.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-# This script compiles Java source code and packs it into a single JAR file.
+# This script compiles the Java source code and packs it into a single JAR file.
 
 set -e  # Exit on error
 # set -x  # Display commands
 
-BUILD_DIR=classes
+JAR_DIR=jar
 PACKAGE=io/multimap
 SOURCE_DIR=src/java
-SOURCE_FILES=($SOURCE_DIR/$PACKAGE/Callables.java \
-              $SOURCE_DIR/$PACKAGE/Check.java \
-              $SOURCE_DIR/$PACKAGE/Iterator.java \
-              $SOURCE_DIR/$PACKAGE/Map.java \
-              $SOURCE_DIR/$PACKAGE/Options.java)
-
-mkdir $BUILD_DIR
-javac -sourcepath $SOURCE_DIR -d $BUILD_DIR ${SOURCE_FILES[*]}
+SOURCE_FILES=($PACKAGE/Callables.java \
+              $PACKAGE/Check.java \
+              $PACKAGE/Iterator.java \
+              $PACKAGE/Map.java \
+              $PACKAGE/Options.java)
 
 MAJOR_VERSION=$(grep MAJOR_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
 MINOR_VERSION=$(grep MINOR_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
 PATCH_VERSION=$(grep PATCH_VERSION src/cpp/multimap/version.hpp | cut -d';' -f1 | cut -d' ' -f6)
 JAR_FILE=multimap-$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION.jar
 
-jar cf $JAR_FILE $BUILD_DIR
-rm -rf $BUILD_DIR
+mkdir $JAR_DIR
+cp -rf $SOURCE_DIR/* $JAR_DIR
+cd $JAR_DIR
+javac ${SOURCE_FILES[*]}
+jar cf ../$JAR_FILE *
+cd ..
+rm -rf $JAR_DIR
