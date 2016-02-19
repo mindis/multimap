105fd99ad4310ad267c727d595f896701b15df39
Martin <martin.trenkmann@posteo.de>
2016-02-19 18:31:21 +0100

    Add implementation

--- src/cpp/multimap/internal/SharedMutex.cpp
+++ src/cpp/multimap/internal/SharedMutex.cpp
@@ -1,5 +1,108 @@
-#include "SharedMutex.hpp"
+// This file is part of Multimap.  http://multimap.io
+//
+// Copyright (C) 2015-2016  Martin Trenkmann
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-SharedMutex::SharedMutex()
-{
+#include "multimap/internal/SharedMutex.hpp"
+
+#include <mutex>
+
+namespace multimap {
+namespace internal {
+
+namespace {
+
+std::mutex shared_mutex_allocation_mutex;
+
+} // namespace
+
+void SharedMutex::lock() {
+  {
+    std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+    if (!mutex_) {
+      mutex_ = allocate();
+    }
+    mutex_->refcount++;
+  }
+  // shared_mutex_allocation_mutex is unlocked here in order to avoid
+  // a deadlock, because the following mutex acquisition might block.
+  mutex_->lock();
 }
+
+bool SharedMutex::try_lock() {
+  std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+  if (!mutex_) {
+    mutex_ = allocate();
+  }
+  const auto success = mutex_->try_lock();
+  if (success) mutex_->refcount++;
+  return success;
+}
+
+void SharedMutex::unlock() {
+  std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+  MT_ASSERT_NOT_ZERO(mutex_->refcount);
+  mutex_->unlock();
+  mutex_->refcount--;
+  if (mutex_->refcount == 0) {
+    deallocate(std::move(mutex_));
+  }
+}
+
+void SharedMutex::lock_shared() {
+  {
+    std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+    if (!mutex_) {
+      mutex_ = allocate();
+    }
+    mutex_->refcount++;
+  }
+  // shared_mutex_allocation_mutex is unlocked here in order to avoid
+  // a deadlock, because the following mutex acquisition might block.
+  mutex_->lock_shared();
+}
+
+bool SharedMutex::try_lock_shared() {
+  std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+  if (!mutex_) {
+    mutex_ = allocate();
+  }
+  const auto success = mutex_->try_lock_shared();
+  if (success) mutex_->refcount++;
+  return success;
+}
+
+void SharedMutex::unlock_shared() {
+  std::lock_guard<std::mutex> lock(shared_mutex_allocation_mutex);
+  MT_ASSERT_NOT_ZERO(mutex_->refcount);
+  mutex_->unlock_shared();
+  mutex_->refcount--;
+  if (mutex_->refcount == 0) {
+    deallocate(std::move(mutex_));
+  }
+}
+
+// Note that this function needs external locking.
+std::unique_ptr<SharedMutex::RefCountedMutex> SharedMutex::allocate() {
+  return std::unique_ptr<RefCountedMutex>(new RefCountedMutex());
+}
+
+// Note that this function needs external locking.
+void SharedMutex::deallocate(std::unique_ptr<RefCountedMutex> mutex) {
+  // Let it die.
+}
+
+}  // namespace internal
+}  // namespace multimap
--- src/cpp/multimap/internal/SharedMutex.hpp
+++ src/cpp/multimap/internal/SharedMutex.hpp
@@ -1,10 +1,67 @@
-#ifndef SHAREDMUTEX_HPP
-#define SHAREDMUTEX_HPP
+// This file is part of Multimap.  http://multimap.io
+//
+// Copyright (C) 2015-2016  Martin Trenkmann
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-class SharedMutex
-{
-public:
-  SharedMutex();
+#ifndef MULTIMAP_INTERNAL_SHARED_MUTEX_HPP_INCLUDED
+#define MULTIMAP_INTERNAL_SHARED_MUTEX_HPP_INCLUDED
+
+#include <memory>
+#include <boost/thread/shared_mutex.hpp>
+#include "multimap/thirdparty/mt/mt.hpp"
+
+namespace multimap {
+namespace internal {
+
+class SharedMutex : public mt::Resource {
+  // This class serves the same purpose as boost::shared_mutex or
+  // std::shared_mutex, but is designed for minimal memory footprint in order
+  // to allow many simultaneous instances. In contrast to the mentioned mutexes
+  // it allocates the actual mutex only on demand from a mutex pool or the free
+  // store, and deallocates it when all locks have been released.
+
+ public:
+  SharedMutex() = default;
+
+  void lock();
+
+  bool try_lock();
+
+  void unlock();
+
+  void lock_shared();
+
+  bool try_lock_shared();
+
+  void unlock_shared();
+
+ private:
+  struct RefCountedMutex : public boost::shared_mutex {
+    uint32_t refcount = 0;
+  };
+
+  static std::unique_ptr<RefCountedMutex> allocate();
+  static void deallocate(std::unique_ptr<RefCountedMutex> mutex);
+
+  std::unique_ptr<RefCountedMutex> mutex_;
 };
 
-#endif // SHAREDMUTEX_HPP
+static_assert(mt::hasExpectedSize<SharedMutex>(4, 8),
+              "class SharedMutex does not have expected size");
+
+}  // namespace internal
+}  // namespace multimap
+
+#endif  // MULTIMAP_INTERNAL_SHARED_MUTEX_HPP_INCLUDED
