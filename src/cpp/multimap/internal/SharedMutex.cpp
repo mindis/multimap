d4fce99cd16eb073e10ab4c736425ce7fb0ff474
Martin <martin.trenkmann@posteo.de>
2016-02-19 18:29:46 +0100

    Initial commit

---
+++ src/cpp/multimap/internal/SharedMutex.cpp
@@ -0,0 +1,5 @@
+#include "SharedMutex.hpp"
+
+SharedMutex::SharedMutex()
+{
+}
---
+++ src/cpp/multimap/internal/SharedMutex.hpp
@@ -0,0 +1,10 @@
+#ifndef SHAREDMUTEX_HPP
+#define SHAREDMUTEX_HPP
+
+class SharedMutex
+{
+public:
+  SharedMutex();
+};
+
+#endif // SHAREDMUTEX_HPP
