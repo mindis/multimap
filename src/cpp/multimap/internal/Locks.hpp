a65a36f5b55b306ad5f3bdc563ce7ec54f309fe6
Martin <martin.trenkmann@posteo.de>
2016-02-20 16:21:16 +0100

    Added implementation

--- src/cpp/multimap/internal/Locks.hpp
+++ src/cpp/multimap/internal/Locks.hpp
@@ -1,4 +1,43 @@
-#ifndef LOCKS_HPP
-#define LOCKS_HPP
+// This file is part of Multimap.  http://multimap.io
+//
+// Copyright (C) 2015-2016  Martin Trenkmann
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#endif // LOCKS_HPP
+#ifndef MULTIMAP_INTERNAL_LOCKS_HPP_INCLUDED
+#define MULTIMAP_INTERNAL_LOCKS_HPP_INCLUDED
+
+#include <mutex>
+#include <boost/thread/lock_types.hpp>
+#include <boost/thread/shared_lock_guard.hpp>
+
+namespace multimap {
+namespace internal {
+
+template <typename SharedMutex>
+using ReaderLock = boost::shared_lock<SharedMutex>;
+
+template <typename SharedMutex>
+using ReaderLockGuard = boost::shared_lock_guard<SharedMutex>;
+
+template <typename SharedMutex>
+using WriterLock = std::unique_lock<SharedMutex>;
+
+template <typename SharedMutex>
+using WriterLockGuard = std::lock_guard<SharedMutex>;
+
+}  // namespace internal
+}  // namespace multimap
+
+#endif  // MULTIMAP_INTERNAL_LOCKS_HPP_INCLUDED
