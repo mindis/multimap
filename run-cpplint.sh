47d224ae5329a6cbfaa23396926c1b099efaf0c6
Martin <martin.trenkmann@posteo.de>
2016-05-07 18:32:26 +0200

    Add paths to internal source files

--- run-cpplint.sh
+++ run-cpplint.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-cpplint --root=src/cpp src/cpp/multimap/*.h src/cpp/multimap/*.cpp
+cpplint --root=src/cpp \
+src/cpp/multimap/*.h \
+src/cpp/multimap/*.cpp \
+src/cpp/multimap/internal/*.h \
+src/cpp/multimap/internal/*.cpp
